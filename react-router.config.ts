e9b2f52644ad49092195983e1a6c8114247328d5
Yahya Anas <yahyaanas2005@gmail.com>
2025-12-29 00:18:30 +0500

    Disable prerendering to fix build error

--- react-router.config.ts
+++ react-router.config.ts
@@ -3,5 +3,6 @@ import type { Config } from '@react-router/dev/config';
 export default {
 	appDirectory: './src/app',
 	ssr: true,
-	prerender: ['/*?'],
+	// Disabled prerendering due to syntax error with catch-all route
+	// prerender: ['/*?'],
 } satisfies Config;
